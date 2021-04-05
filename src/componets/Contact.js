ffb5d71ba07e92b115e656b1820dd6e4ae136271
travis-racisz <63677846+travis-racisz@users.noreply.github.com>
2021-04-05 17:30:11 +0900

    Update Contact.js

--- src/componets/Contact.js
+++ src/componets/Contact.js
@@ -7,7 +7,7 @@ export default function Contact(){
 
                 <h2  className ="contact">Contact</h2>
                 <a href = "mailto:travis@devlabdesigns.io">Travis@devlabdesigns.io</a>
-                <p className = "phone-number">+1 939 200 9954</p>
+                <a href = "tel:+19392009954" className = "phone-number">+1 939 200 9954</a>
             </div>
         </div>
     )
